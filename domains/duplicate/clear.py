33dcb9bf90a3d7d3aa36ea93eb9e30bc0526ba58
tw1ns <tw1ns.famgor@gmail.com>
2015-09-09 07:07:43 +0000

    modified:   domains/brut/domains.txt 	modified:   domains/brut/good.txt 	modified:   domains/brut/pwd.txt 	renamed:    domains/brut/all.txt -> domains/brut/source.txt 	modified:   domains/check/domains.txt 	deleted:    domains/check/error.txt 	modified:   domains/check/good.txt 	renamed:    domains/check/good.txt -> domains/check/wp_rf.txt 	new file:   domains/duplicate/clear.py 	modified:   domains/pars/domains.txt 	modified:   domains/pars/request.txt 	deleted:    domains/pars/url_pars.txt

---
+++ domains/duplicate/clear.py
@@ -0,0 +1,18 @@
+import os
+def f7(seq):
+    seen = set()
+    seen_add = seen.add
+    return [ x for x in seq if not (x in seen or seen_add(x))]
+
+print('Removing duplicates...')
+input = open('good.txt', 'r')
+output = open('good_clear.txt', 'w')
+
+linesarray = input.readlines()
+input.close()
+seen = []
+seen = f7(linesarray)
+for i in range(len(seen)):
+    output.write(seen[i])
+output.close()
+print('Complete')
